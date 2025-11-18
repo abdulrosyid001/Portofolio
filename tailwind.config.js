3e8deb9ac53d51d592c36b6c088bba7d1de5db5f
Abdul Rosyid <abdulrosyid6122004@gmail.com>
2025-11-18 15:35:29 +0000

    Error

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+      },
       colors: {
         primary: '#7C3AED',
         purpleDeep: '#6B21A8',
