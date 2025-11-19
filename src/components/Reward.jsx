404ced8560b9d31a16e5afbcd9c99e50be5a28af
Abdul Rosyid <abdulrosyid6122004@gmail.com>
2025-11-19 11:45:16 +0000

    Edit penghargaan

--- src/components/Reward.jsx
+++ src/components/Reward.jsx
@@ -3,37 +3,37 @@ import React from 'react'
 // Awards: list of achievements / recognitions
 const awards = [
   { 
-    title: 'Best Speaker Esai Pesta Ilmiah Sriwijaya',
+    title: 'Best Speaker Esai - Pesta Ilmiah Sriwijaya',
     organizer: 'Universitas Sriwijaya',
     year: '2024' 
   },
   { 
-    title: 'Juara 1 Karya Tulis Ilmiah Jambore Statistika',
+    title: 'Juara 1 Karya Tulis Ilmiah - Jambore Statistika',
     organizer: 'Universitas Mulawarman',
     year: '2025' 
   },
   { 
-    title: 'Pembiayaan WIBAWA',
+    title: 'Pembiayaan Business Plan - WIBAWA',
     organizer: 'Universitas Sebelas Maret',
     year: '2025' 
   },
   { 
-    title: 'Juara 3 Data Science Padjadjaran Statistics Olympiad',
+    title: 'Juara 3 Data Science - Padjadjaran Statistics Olympiad',
     organizer: 'Universitas Padjajaran',
     year: '2025' 
   },
   { 
-    title: '15 besar Esai Satria Data',
+    title: '15 Besar Esai - Satria Data',
     organizer: 'Pusat Prestasi Nasional',
     year: '2025' 
   },
   { 
-    title: 'Posisi 4 Data Science Sebelas Maret Statistics Data Science',
+    title: 'Posisi 4 Data Science - Sebelas Maret Statistics Data Science',
     organizer: 'Universitas Sebelas Maret',
     year: '2025' 
   },
   { 
-    title: 'Posisi 11 Data Science Data Quest',
+    title: 'Posisi 11 Data Science - Data Quest',
     organizer: 'Universitas Airlangga',
     year: '2025'
   }
